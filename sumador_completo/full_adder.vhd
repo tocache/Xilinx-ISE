66dafdaed0bc52659a7ca5b84cad11241fbd66f3
Kalun Lau <klnlau@gmail.com>
2018-09-21 18:35:22 -0500

    Add files via upload

---
+++ sumador_completo/full_adder.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use IEEE.std_logic_unsigned.all;
+
+
+entity full_adder is
+    Port ( Cin : in  STD_LOGIC;
+           A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Cout : out  STD_LOGIC;
+           Suma : out  STD_LOGIC);
+end full_adder;
+
+architecture Behavioral of full_adder is
+
+begin
+
+	Suma <= (A xor B) xor Cin;
+	Cout <= (A and B) or ((A xor B) and Cin);
+
+end Behavioral;
+
